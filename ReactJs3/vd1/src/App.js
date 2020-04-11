4c4e27df45c609366be0cd98466de657d9ec55f1
sieuphongdo <hieutrandn9889@gmail.com>
2020-04-11 20:44:50 +0700

    update git and app

--- ReactJs3/vd1/src/App.js
+++ ReactJs3/vd1/src/App.js
@@ -1,6 +1,8 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const a1="hieu";
+// khai bao theo ham bt
 function One() {
   return (
     <div>
@@ -8,18 +10,24 @@ function One() {
     </div>
   )
 }
-var Two = function(){
-  return(
+
+// khai bao theo anymomous function
+var Two = function () {
+  return (
     <div>
       <h1> Kieu anynomous function</h1>
     </div>
   )
 }
+
+//Khai bao theo es6
 var Three = () => (
   <div>
     <h1>Kieu es6</h1>
   </div>
 )
+
+// khai bao theo class
 class Four extends Component {
   render() {
     return (
@@ -29,26 +37,58 @@ class Four extends Component {
     );
   }
 }
-var UseProps = (props) => (
+
+// su dung props cach 1
+var UsePropsOne = (props) => (
   <div>
-      <h1>{props.tieuDe}</h1>
-      <img className="card-img-top" src={props.linkAnh} alt=""/>
-      
+    <h1>{props.tieuDe}</h1>
+    <img className="card-img-top" src={props.linkAnh} alt="" />
   </div>
 )
+//  su dung props cach 2 >> su dung class de dinh nghia va thao tac voi props
+class UsePropsTwo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.tieuDe}</h1>
+        <img className="card-img-top" src={this.props.linkAnh} alt="" />
+      </div>
+    );
+  }
+}
+
+// JSX
+class App1 extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="nut">Ten tui la: {a1}</h3>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input" name id defaultValue="checkedValue" defaultChecked />
+            Display value
+          </label>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> 
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <UseProps tieuDe="Dich Benh" linkAnh="https://kenh14cdn.com/2020/4/7/9259164428738153793622975906720805378588672n-1586265443630314075999.jpg"/>
-        <One/>
-        <Two/>
-        <Three/>
-        <Four/>
+        <UsePropsOne tieuDe="Dung props theo cach 1 su dung function" linkAnh="https://kenh14cdn.com/2020/4/7/9259164428738153793622975906720805378588672n-1586265443630314075999.jpg" />
+        <UsePropsTwo tieuDe="Dung props theo cach 2 su dung class" linkAnh="https://kenh14cdn.com/2020/4/7/9259164428738153793622975906720805378588672n-1586265443630314075999.jpg" />
+        <One />
+        <Two />
+        <Three />
+        <Four />
+        <App1/>
         <a
           className="App-link"
           href="https://reactjs.org"

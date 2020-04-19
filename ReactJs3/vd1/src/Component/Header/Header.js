2d18d357958fe12a81e08b209b4c874c83cf5181
Hiếu Văn Trần <hieu.tran@enouvo.com>
2020-04-19 15:10:59 +0700

    text

--- ReactJs3/vd1/src/Component/Header/Header.js
+++ ReactJs3/vd1/src/Component/Header/Header.js
@@ -7,9 +7,9 @@ class Header extends Component {
                  <header className="masthead text-center text-white">
                     <div className="masthead-content">
                     <div className="container">
-                        <h1 className="masthead-heading mb-0">One Page Wonder</h1>
-                        <h2 className="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
-                        <a href="#" className="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>
+                        <h1 className="masthead-heading mb-0">That is a website to learn ReactJS and NodeJS by hieutran</h1>
+                        <h2 className="masthead-subheading mb-0">Out of scope for tester :v</h2>
+                        <a href="#" className="btn btn-primary btn-xl rounded-pill mt-5">More Learning</a>
                     </div>
                     </div>
                     <div className="bg-circle-1 bg-circle" />
